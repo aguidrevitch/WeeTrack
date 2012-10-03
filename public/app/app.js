3bab19099f123340c173fd6153312b7d374084c7
Aleksandr Guidrevitch <aguidrevitch@gmail.com>
2012-10-03 16:56:00 +0300

    chaining, password verification

--- public/app/app.js
+++ public/app/app.js
@@ -84,18 +84,16 @@ define([
                 }, additionalProps);
             },
             
-            start: function () {
+            start: _.once(function () {
                 ready.done(function () {
-                    _.once(function () {
-                        // Trigger the initial route and enable HTML5 History API support, set the
-                        // root folder to '/' by default.  Change in app.js.
-                        Backbone.history.start({
-                            pushState: true, 
-                            root: app.root
-                        });
-                    })();
+                    // Trigger the initial route and enable HTML5 History API support, set the
+                    // root folder to '/' by default.  Change in app.js.
+                    Backbone.history.start({
+                        pushState: true, 
+                        root: app.root
+                    });
                 });
-            },
+            }),
             
             // Helper for using layouts.
             useLayout: function(name, options) {
