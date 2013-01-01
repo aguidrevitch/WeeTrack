44f5332517a0680955121c15f13b1d245621512f
Aleksandr Guidrevitch <aguidrevitch@gmail.com>
2013-01-01 19:10:39 +0300

    minor file layout update

--- public/assets/js/plugins/jquery.serializeObject.js
+++ public/assets/js/plugins/jquery.serializeObject.js
@@ -10,16 +10,22 @@
 }(function ($) {
     'use strict';
     $.fn.serializeObject = function() {
+        var form = this;
         var o = {};
-        var a = this.serializeArray();
+        var a = form.serializeArray();
         $.each(a, function() {
-            if (o[this.name]) {
-                if (!o[this.name].push) {
-                    o[this.name] = [o[this.name]];
-                }
-                o[this.name].push(this.value || '');
+            var s2 = form.find('[name=' + this.name + ']:input').data('select2');
+            if (s2) {
+                o[this.name] = s2.val();
             } else {
-                o[this.name] = this.value || '';
+                if (o[this.name]) {
+                    if (!o[this.name].push) {
+                        o[this.name] = [o[this.name]];
+                    }
+                    o[this.name].push(this.value || '');
+                } else {
+                    o[this.name] = this.value || '';
+                }
             }
         });
         return o;
